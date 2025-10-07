df622149a3ef9b4cddd81573007e1aec95914232
sidd-gupta05 <siddharthgupta2482005@gmail.com>
2025-10-07 15:54:52 +0530

    db_fixes

--- prisma/migrations/20251005214756_add_outbreak_report/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[labId]` on the table `outbreak_reports` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "outbreak_reports_labId_key" ON "outbreak_reports"("labId");
