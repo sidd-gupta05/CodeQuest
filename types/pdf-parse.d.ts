64c84a8681277158484b1110d0b2dc4f61329521
krrishmahar <krrishmahar5@gmail.com>
2025-10-19 13:06:53 +0530

    writing prettier changes

--- types/pdf-parse.d.ts
+++ types/pdf-parse.d.ts
@@ -1,26 +1,27 @@
 declare module 'pdf-parse' {
-    interface PDFInfo {
-        PDFFormatVersion?: string;
-        IsAcroFormPresent?: boolean;
-        IsXFAPresent?: boolean;
-        [key: string]: unknown;
-    }
+  interface PDFInfo {
+    PDFFormatVersion?: string;
+    IsAcroFormPresent?: boolean;
+    IsXFAPresent?: boolean;
+    [key: string]: unknown;
+  }
 
-    interface PDFMetadata {
-        [key: string]: unknown;
-    }
+  interface PDFMetadata {
+    [key: string]: unknown;
+  }
 
-    interface PDFParseResult {
-        numpages: number;
-        numrender: number;
-        info: PDFInfo;
-        metadata?: PDFMetadata;
-        text: string;
-        version: string;
-    }
+  interface PDFParseResult {
+    numpages: number;
+    numrender: number;
+    info: PDFInfo;
+    metadata?: PDFMetadata;
+    text: string;
+    version: string;
+  }
 
-    function pdf(input: Buffer, options?: Record<string, unknown>): Promise<PDFParseResult>;
-    export default pdf;
+  function pdf(
+    input: Buffer,
+    options?: Record<string, unknown>
+  ): Promise<PDFParseResult>;
+  export default pdf;
 }
-
-
--- types/pdfjs-dist.d.ts
+++ types/pdfjs-dist.d.ts
@@ -1,5 +1,5 @@
 // types/pdfjs-dist.d.ts
-declare module "pdfjs-dist/legacy/build/pdf.js" {
+declare module 'pdfjs-dist/legacy/build/pdf.js' {
   export const getDocument: any;
   export const GlobalWorkerOptions: any;
 }
